1df8d048696084d48152aea55fe419c5bb7bbf61
ElvenWhiskers <Bringel.Brittany@Gmail.com>
2023-11-14 02:13:49 -0800

    attempting to redo grass, issues included.

---
+++ src/main/java/com/elvenwhiskers/starfall/block/custom/ModGrassBlock.java
@@ -0,0 +1,81 @@
+package com.elvenwhiskers.starfall.block.custom;
+
+import com.elvenwhiskers.starfall.block.ModBlocks;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.BonemealableBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.RandomPatchConfiguration;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ModGrassBlock implements BonemealableBlock {
+
+
+
+
+    @Override
+    public boolean isValidBonemealTarget(LevelReader pLevel, BlockPos pPos, BlockState pState, boolean pIsClient) {
+        return pLevel.getBlockState(pPos.above()).isAir();
+    }
+
+    @Override
+    public boolean isBonemealSuccess(Level pLevel, RandomSource pRandom, BlockPos pPos, BlockState pState) {
+        return false;
+    }
+
+    @Override
+    public void performBonemeal(ServerLevel pLevel, RandomSource pRandom, BlockPos pPos, BlockState pState) {
+        BlockPos blockpos = pPos.above();
+        //BlockState blockstate = ModBlocks.STARCROSSED_GRASS_BLOCK.defaultBlockState();
+        Optional<Holder.Reference<PlacedFeature>> optional = pLevel.registryAccess().registryOrThrow(Registries.PLACED_FEATURE).getHolder(VegetationPlacements.GRASS_BONEMEAL);
+
+        label49:
+        for(int i = 0; i < 128; ++i) {
+            BlockPos blockpos1 = blockpos;
+
+            for(int j = 0; j < i / 16; ++j) {
+                blockpos1 = blockpos1.offset(pRandom.nextInt(3) - 1, (pRandom.nextInt(3) - 1) * pRandom.nextInt(3) / 2, pRandom.nextInt(3) - 1);
+                if (!pLevel.getBlockState(blockpos1.below()).is(this) || pLevel.getBlockState(blockpos1).isCollisionShapeFullBlock(pLevel, blockpos1)) {
+                    continue label49;
+                }
+            }
+
+            BlockState blockstate1 = pLevel.getBlockState(blockpos1);
+            if (blockstate1.is(blockstate.getBlock()) && pRandom.nextInt(10) == 0) {
+                ((BonemealableBlock)blockstate.getBlock()).performBonemeal(pLevel, pRandom, blockpos1, blockstate1);
+            }
+
+            if (blockstate1.isAir()) {
+                Holder<PlacedFeature> holder;
+                if (pRandom.nextInt(8) == 0) {
+                    List<ConfiguredFeature<?, ?>> list = pLevel.getBiome(blockpos1).value().getGenerationSettings().getFlowerFeatures();
+                    if (list.isEmpty()) {
+                        continue;
+                    }
+
+                    holder = ((RandomPatchConfiguration)list.get(0).config()).feature();
+                } else {
+                    if (!optional.isPresent()) {
+                        continue;
+                    }
+
+                    holder = optional.get();
+                }
+
+                holder.value().place(pLevel, pLevel.getChunkSource().getGenerator(), pRandom, blockpos1);
+            }
+        }
+
+    }
+}
